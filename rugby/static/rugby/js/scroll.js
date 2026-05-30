e225a4ef8a42ea6275cb6736a9b67c0b404a6cd5
walery <waleron@gmail.com>
2026-05-31 00:46:53 +0200

    refactor(rugby): adjust styling to home page style

---
+++ rugby/static/rugby/js/scroll.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  const navbar = document.querySelector('.navbar');
+  if (!navbar) return;
+
+  function onScroll() {
+    if (window.scrollY > 40) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
+
+  window.addEventListener('scroll', onScroll, { passive: true });
+  onScroll(); // run once on load in case page is already scrolled
+})();
--- rugby/urls.py
+++ rugby/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+
 from analytics.utils import count_visit
+
+from . import views
 
 urlpatterns = [
     path('', count_visit(views.post_list), name='post_list'),
