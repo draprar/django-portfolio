0197c076d36079982d0ffd572b8f41251fcfacbd
walery <waleron@gmail.com>
2026-06-13 18:42:25 +0200

    feat(wyraj): add 7 fests

--- bies/urls.py
+++ bies/urls.py
@@ -6,4 +6,10 @@ app_name = "bies"
 urlpatterns = [
     path("", views.wyraj_lista, name="wyraj-lista"),
     path("swietowit-letni/", views.wyraj_swietowit_letni, name="wyraj-swietowit-letni"),
+    path("gaik/", views.wyraj_gaik, name="wyraj-gaik"),
+    path("radonica/", views.wyraj_radonica, name="wyraj-radonica"),
+    path("smigus-dyngus/", views.wyraj_smigus_dyngus, name="wyraj-smigus-dyngus"),
+    path("jare-gody/", views.wyraj_jare_gody, name="wyraj-jare-gody"),
+    path("miesopust/", views.wyraj_miesopust, name="wyraj-miesopust"),
+    path("szczodre-gody/", views.wyraj_szczodre_gody, name="wyraj-szczodre-gody"),
 ]
--- bies/views.py
+++ bies/views.py
@@ -11,3 +11,27 @@ def wyraj_lista(request):
 @count_visit
 def wyraj_swietowit_letni(request):
     return render(request, "bies/wyraj/swietowit-letni/index.html")
+
+@count_visit
+def wyraj_gaik(request):
+    return render(request, "bies/wyraj/gaik/index.html")
+
+@count_visit
+def wyraj_radonica(request):
+    return render(request, "bies/wyraj/radonica/index.html")
+
+@count_visit
+def wyraj_smigus_dyngus(request):
+    return render(request, "bies/wyraj/smigus-dyngus/index.html")
+
+@count_visit
+def wyraj_jare_gody(request):
+    return render(request, "bies/wyraj/jare-gody/index.html")
+
+@count_visit
+def wyraj_miesopust(request):
+    return render(request, "bies/wyraj/miesopust/index.html")
+
+@count_visit
+def wyraj_szczodre_gody(request):
+    return render(request, "bies/wyraj/szczodre-gody/index.html")
